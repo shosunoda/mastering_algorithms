4faa28ad12809ac7a18f2bfa62e71ffdd7298302
Sho <tsunodasho@berkeley.edu>
2025-11-15 03:01:43 +0900

    basic_cauclator2

---
+++ topics/mediums/basicCalculator2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        prev_operator = '+'
+        s = [char for char in s if char != " "]
+        
+        for i in range(len(s) + 1):
+            ch = s[i] if i < len(s) else '\0'
+            
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            
+            if not ch.isdigit() or i == len(s):
+                if prev_operator == '+':
+                    stack.append(num)
+                if prev_operator == '-':
+                    stack.append(-num)
+                if prev_operator == '*':
+                    stack.append(stack.pop() * num)
+                if prev_operator == '/':
+                    stack.append(int(stack.pop() / num))
+                
+                prev_operator = ch
+                num = 0
+        
+        return sum(stack)
