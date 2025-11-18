d8e2b582a7af44a6c278da9ce37fbc2211654467
Sho <tsunodasho@berkeley.edu>
2025-11-19 02:31:15 +0900

    hi

---
+++ topics/mediums/Jump Game II.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        #[2,3,1,1,4]
+        # 
+        l = r = 0
+        jumps = 0
+        furthest = 0
+        while r < len(nums) - 1:
+            for hop in range(l, r + 1):
+                furthest = max(furthest, hop + nums[hop])
+            l = r + 1
+            r = furthest
+            jumps += 1
+        return jumps
+
+
+        
